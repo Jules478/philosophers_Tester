d8517d604314f56915f3ff3ecb02a349a341ec27
Martin Pierce <mpierce@c1r2p1.hive.fi>
2025-03-11 17:16:57 +0200

    fixes

--- test.sh
+++ test.sh
@@ -5,21 +5,18 @@ RED='\e[1;31m'
 WHITE='\e[1;37m'
 RESET='\033[0m'
 
-echo -n > julestestfile
-echo -e "139" > julessegcheck
-
 run_err() 
 {
 	local test_desc=$1
 	shift
-	> julestestfile
-	> julestestout
-	./philo $@ > julestestout 2> julesstderr
+	> .julestestfile
+	> .julestestout
+	./philo $@ > .julestestout 2> .julesstderr
 	exit_code=$?
 	if [ $exit_code -eq 139 ]; then
 		echo -n "❌"
 		echo -e "$test_desc: Segmentation Fault\n" >> philo_trace
-	elif [ ! -s julestestout ] && [ ! -s julesteststderr ]; then
+	elif [ ! -s .julestestout ] && [ ! -s .julesstderr ]; then
 		echo -n "❌"
 		echo -e "$test_desc: No error message found\n" >> philo_trace
 	else
@@ -33,13 +30,13 @@ run_one()
 	local min=$3
 	local max=$((min + 10))
 	shift
-	> julestestout
-	> julesone
-	./philo $@ 1> julesone 2> /dev/null
-	time=$(tail -n 1 julesone | awk '{print $1}')
+	> .julestestout
+	> .julesone
+	./philo $@ 1> .julesone 2> /dev/null
+	time=$(tail -n 1 .julesone | awk '{print $1}')
 	if ((time >= min)) && ((time <= max)); then
-		tail -n 1 julesone | sed 's/[0-9]\+ //' > julestestout
-		if diff julestestout julestestfile > /dev/null; then
+		tail -n 1 .julesone | sed 's/[0-9]\+ //' > .julestestout
+		if diff .julestestout .julestestfile > /dev/null; then
 			echo -n "✅"
 		else
 			echo -n "❌"
@@ -59,12 +56,12 @@ run_full()
 	local time_sleep=$5
 	local eat=$6
 	local forks=$(( eat * philo * 2))
-	local tolerance=2
+	local tolerance=1
 	shift
-	> julestestout
-	> julesphilolog
-	./philo $@ 1> julestestout 2> /dev/null
-	awk '$2 == "1"' julestestout > julesphilolog
+	> .julestestout
+	> .julesphilolog
+	./philo $@ 1> .julestestout 2> /dev/null
+	awk '$2 == "1"' .julestestout > .julesphilolog
 	read eat_time sleep_time think_time < <(awk '
     $4 == "eating" && !eat_time {
         eat_time = $1
@@ -77,26 +74,25 @@ run_full()
         print eat_time, sleep_time, think_time
         exit
     }
-	' julesphilolog)
-	if grep -q " died" julestestout; then
+	' .julesphilolog)
+	if grep -q " died" .julestestout; then
 		echo -n "❌"
 		echo -e "$test_desc: Philosopher died\n" >> philo_trace
-	elif [ "$(grep "is eating" julestestout | wc -l)" -lt $eat ]; then
+	elif [ "$(grep "is eating" .julestestout | wc -l)" -lt $eat ]; then
 		echo -n "❌"
 		echo -e "$test_desc: Philosophers did not eat enough\n" >> philo_trace
-	elif [ "$(grep "has taken a fork" julestestout | wc -l)" -lt $forks ]; then
+	elif [ "$(grep "has taken a fork" .julestestout | wc -l)" -lt $forks ]; then
 		echo -n "❌"
 		echo -e "$test_desc: Philosophers did not take enough forks\n" >> philo_trace
-	elif [ "$(grep "is sleeping" julestestout | wc -l)" -lt $eat ]; then
+	elif [ "$(grep "is sleeping" .julestestout | wc -l)" -lt $eat ]; then
 		echo -n "❌"
 		echo -e "$test_desc: Philosophers did not sleep enough\n" >> philo_trace
 	elif ((sleep_time - eat_time < time_eat || sleep_time - eat_time > time_eat + tolerance)); then
 		echo -n "❌"
-		echo -e "$test_desc: Philosophers did not spend enough time eating\n" >> philo_trace
+		echo -e "$test_desc: Philosophers did not eat for the correct time\n" >> philo_trace
 	elif ((think_time - sleep_time < time_sleep || think_time - sleep_time > time_sleep + tolerance)); then
 		echo -n "❌"
-		echo -e "$test_desc: Philosophers did not spend enough time sleeping\n" >> philo_trace
-		echo -e "desc: $test_desc philo: $philo time_eat: $time_eat time_sleep: $time_sleep eat: $eat"
+		echo -e "$test_desc: Philosophers did not sleep for the correct time\n" >> philo_trace
 	else
 		echo -n "✅"
 	fi
@@ -112,26 +108,24 @@ run_death()
 	local time_sleep=$5
 	local eat=$6
 	shift
-	> julestestout
-	> julesphilolog
-	./philo $@ 1> julestestout 2> /dev/null
-	time=$(tail -n 1 julestestout | awk '{print $1}')
-	dead_id=$(awk '$3 == "died" {id = $2} END {print id}' julestestout)
-	awk -v id="$dead_id" '$2 == id' julestestout > julesdeathlog
-	last_meal=$(awk '$3 == "eating" {time = $1} END {print time}' julesdeathlog)
+	> .julestestout
+	> .julesphilolog
+	./philo $@ 1> .julestestout 2> /dev/null
+	time=$(tail -n 1 .julestestout | awk '{print $1}')
+	dead_id=$(awk '$3 == "died" {id = $2} END {print id}' .julestestout)
+	awk -v id="$dead_id" '$2 == id' .julestestout > .julesdeathlog
+	last_meal=$(awk '$3 == "eating" {time = $1} END {print time}' .julesdeathlog)
 	: "${last_meal:=0}"
-	awk '$2 == "1"' julestestout > julesphilolog
+	awk '$2 == "1"' .julestestout > .julesphilolog
 	variance=$((time - last_meal))
-	if ((variance >= min)) && ((variance <=max)); then
-		if ! grep -q " died" julestestout; then
-			echo -n "❌"
-			echo -e "$test_desc: Philosopher did not die die\n" >> philo_trace
-		else
-			echo -n "✅"
-		fi
-	else
+	if ! tail -n 1 .julestestout | grep -q "died"; then
 		echo -n "❌"
-		echo -e "$test_desc philosopher did not die on time\n" >> philo_trace
+		echo -e "$test_desc: Philosopher did not die\n" >> philo_trace
+	elif ((variance < min)) && ((variance > max)); then
+		echo -n "❌"
+		echo -e "$test_desc: Philosopher did not die on time\n" >> philo_trace
+	else
+		echo -n "✅"
 	fi
 }
 
@@ -150,6 +144,8 @@ echo -e "----- TRACE BEGINS -----\n" >> philo_trace
 echo -e "${PURPLE}--- ${WHITE}Basic Error Tests${PURPLE} ---\n${RESET}"
 echo -e "-- Basic Error Tests --\n" >> philo_trace
 
+echo -n > .julestestfile
+
 run_err "No arguments" ""
 run_err "1" 1
 run_err "1 1" 1 1
@@ -167,11 +163,12 @@ run_err "1 0 1 1" 1 0 1 1
 run_err "1 1 0 1" 1 1 0 1
 run_err "1 1 1 0" 1 1 1 0
 run_err "1 1 1 1 0" 1 1 1 1 0
+rm -rf .julestestfile .julestestout .julesstderr
 
 echo -e "${PURPLE}\n\n--- ${WHITE}One Philosopher Tests${PURPLE} ---\n${RESET}"
 echo -e "-- One Philosopher Tests --\n" >> philo_trace
 
-echo -e "1 died" > julestestfile
+echo -e "1 died" > .julestestfile
 run_one "1 1 1 1 1" 1 1 1 1 1
 run_one "1 2 1 1 2" 1 2 1 1 2
 run_one "1 10 5 5 2" 1 10 5 5 2
@@ -179,6 +176,7 @@ run_one "1 50 10 10 2" 1 500 10 10 2
 run_one "1 50 10 10 10" 1 50 10 10 10
 run_one "1 100 25 25 2" 1 100 25 25 2
 run_one "1 100 150 150 10" 1 100 150 150 10
+rm -rf .julestestout .julesone .julestestfile
 
 echo -e "${PURPLE}\n\n--- ${WHITE}No Death Tests${PURPLE} ---\n${RESET}"
 echo -e "-- No Death Tests --\n" >> philo_trace
@@ -198,6 +196,8 @@ run_full "5 100 25 25 15" 5 100 25 25 15
 run_full "10 50 10 10 50" 10 50 10 10 50
 run_full "11 900 150 90 20" 11 900 150 90 20
 
+rm -rf .julestestout .julesphilolog
+
 echo -e "${PURPLE}\n\n--- ${WHITE}Death Tests${PURPLE} ---\n${RESET}"
 echo -e "-- Death Tests --\n" >> philo_trace
 
@@ -209,5 +209,5 @@ run_death "4 190 100 100 5" 4 190 100 100 5
 run_death "5 1 1 1 3" 5 1 1 1 3
 run_death "10 200 100 100 10" 10 200 100 100 10
 echo -e "\n"
-rm -rf jules*
+rm -rf .julestestout .julesphilolog .julesdeathlog
 echo -e "---- TRACE ENDS ----\n" >> philo_trace
